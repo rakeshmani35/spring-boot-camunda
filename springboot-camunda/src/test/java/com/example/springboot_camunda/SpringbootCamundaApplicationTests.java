05c5f4b7258b900c90ae76ff6e3aa0831c18243f
rakeshmani35 <rakeshmani35@gmail.com>
2025-01-08 19:55:06 +0530

    spring boot camunda example

---
+++ springboot-camunda/src/test/java/com/example/springboot_camunda/SpringbootCamundaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_camunda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCamundaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
